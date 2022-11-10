6bf8ae18ac215e4989be68b2883a03c0a132f464
mape86 <90511658+mape86@users.noreply.github.com>
2022-11-10 23:19:29 +0100

    Added links in app.tsx for testing. Links may be moved into the respective pages later.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {HomePage, CharacterPage, ConsolePage, PlagueTalePage, TheLastOfUsPage, TheWitcherPage, TombRaiderPage, UnchartedPage} from './pages/index'
 
 function App() {
   return (
+    <>
+    <BrowserRouter>
     <div>
       <h1>Hello teammates!</h1>
     </div>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="CharacterPage">Characters</Link></li>
+        <li><Link to="ConsolePage">Consoles</Link></li>
+        <li><Link to="PlagueTalePage">Plague Tale</Link></li>
+        <li><Link to="TheLastOfUsPage">The Last of Us</Link></li>
+        <li><Link to="TheWitcherPager">The Witcher 3</Link></li>
+        <li><Link to="TombRaiderPage">Shadow of the Tomb Raider</Link></li>
+        <li><Link to="UnchartedPage">Uncharted 4</Link></li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path='/' element={<HomePage/>}></Route>
+      <Route path='CharacterPage' element={<CharacterPage/>}></Route>
+      <Route path='ConsolePage' element={<ConsolePage/>}></Route>
+      <Route path='PlagueTalePage' element={<PlagueTalePage/>}></Route>
+      <Route path='TheLastOfUsPage' element={<TheLastOfUsPage/>}></Route>
+      <Route path='TheWitcherPager' element={<TheWitcherPage/>}></Route>
+      <Route path='TombRaiderPage' element={<TombRaiderPage/>}></Route>
+      <Route path='UnchartedPage' element={<UnchartedPage/>}></Route>
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
