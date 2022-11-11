3b39a11f541101dbcc86e1d0fb300f6dc84350fb
mape86 <90511658+mape86@users.noreply.github.com>
2022-11-11 12:44:48 +0100

    Component Items: Added items for Console, Character and Game.

---
+++ src/components/characters/CharacterItem.tsx
@@ -0,0 +1,10 @@
+import ICharacter from "../../interfaces/ICharacter"
+import {FC} from 'react';
+
+const CharacterItem: FC<ICharacter> = ({id, name, gender, game, description, image}) => {
+    return (
+        <article></article>
+    )
+}
+
+export default CharacterItem
---
+++ src/components/consoles/ConsoleItem.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import IConsole from "../../interfaces/IConsole";
+
+const ConsoleItem: FC<IConsole> = ({id, model, brand, price, description, image}) => {
+    
+    return (
+        <article></article>
+    )
+}
+
+export default ConsoleItem
