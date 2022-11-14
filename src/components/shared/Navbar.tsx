cb4bc124a8eaebadc678ee2675607f369a4e2d81
Marianneind <marianne.indrebo@gmail.com>
2022-11-14 21:26:54 +0100

    feat: create file for all games page and update nav

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,18 +1,44 @@
-import {Link} from 'react-router-dom'
-import './Navbar.css'
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <nav className='navbar navbar-expand-lg navbar-bg'>
-          
-            <div className='d-flex justify-content-around'>
-              <div><Link className='btn btn-large btn-rounded btn-outline-light' to='/'> Home </Link></div>
-              <div><Link className='btn btn-large btn-rounded btn-outline-light' to='ConsolePage'> Consoles </Link></div>
-              <div><Link className='btn btn-large btn-rounded btn-outline-light' to='CharacterPage'>Characters</Link></div>
-            </div>
-          
-        </nav>
-      )
-} 
+  return (
+    <nav className="navbar navbar-expand-lg navbar-bg">
+      <div className="d-flex justify-content-around">
+        <div>
+          <Link className="btn btn-large btn-rounded btn-outline-light" to="/">
+            {" "}
+            Home{" "}
+          </Link>
+        </div>
+        <div>
+          <Link
+            className="btn btn-large btn-rounded btn-outline-light"
+            to="ConsolePage"
+          >
+            {" "}
+            Consoles{" "}
+          </Link>
+        </div>
+        <div>
+          <Link
+            className="btn btn-large btn-rounded btn-outline-light"
+            to="CharacterPage"
+          >
+            Characters
+          </Link>
+        </div>
+        <div>
+          <Link
+            className="btn btn-large btn-rounded btn-outline-light"
+            to="AllGamesPage"
+          >
+            All games
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar; 
+export default Navbar;
