bb939dc640161c2e7b679168fa33d218aa2ad663
mape86 <90511658+mape86@users.noreply.github.com>
2022-11-14 20:25:38 +0100

    Edit navbar: Edited Links, added flex and space between.

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -4,13 +4,13 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav className='navbar navbar-expand-lg navbar-bg'>
+        <nav className='d-flex justify-content-around navbar navbar-custom'>
           
-            <div className='d-flex justify-content-around'>
-              <div><Link className='btn btn-large btn-rounded btn-outline-light' to='/'> Home </Link></div>
-              <div><Link className='btn btn-large btn-rounded btn-outline-light' to='ConsolePage'> Consoles </Link></div>
-              <div><Link className='btn btn-large btn-rounded btn-outline-light' to='CharacterPage'>Characters</Link></div>
-            </div>
+            
+              <Link className='text-decoration-none' to='/'> Home </Link>
+              <Link className='btn btn-large btn-rounded btn-outline-light' to='ConsolePage'> Consoles </Link>
+              <Link className='btn btn-large btn-rounded btn-outline-light' to='CharacterPage'>Characters</Link>
+            
           
         </nav>
       )
