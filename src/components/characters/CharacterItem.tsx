537d49dcc4abe00bc095b69ebea0d7416c92a0b8
mape86 <90511658+mape86@users.noreply.github.com>
2022-11-16 14:42:11 +0100

    Added card: Added card in components/shared to be used on character page. May also be used in alle games page?

--- src/components/characters/CharacterItem.tsx
+++ src/components/characters/CharacterItem.tsx
@@ -1,14 +1,16 @@
 import ICharacter from "../../interfaces/ICharacter"
 import {FC} from 'react';
+import Card from "../shared/Card";
 
 const CharacterItem: FC<ICharacter> = ({id, name, gender, game, description, image}) => {
+    /*TODO: Add images and fix image URL (<img />)*/ 
     return (
-        <article>
+        <Card>
             <h3>{name} - ID: {id}</h3>
             <p>A {gender}, appearing in {game}.</p>
             <p><i>"{description}"</i></p>
             <div>{image}</div>
-        </article>
+        </Card>
     )
 }
 
