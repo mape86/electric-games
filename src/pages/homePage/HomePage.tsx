bb06fd5fbeed302a3975d8b2ed972fd942190b2a
Marianneind <marianne.indrebo@gmail.com>
2022-11-17 14:10:29 +0100

    added images to homepage + snapp-effect

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,57 +1,59 @@
 import { Link } from "react-router-dom";
 import { FaInstagram, FaYoutube, FaTwitter } from "react-icons/fa";
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <>
-      <section>
-        <h1>Assasins Creed</h1>
-        <Link to="AssassinsCreed">Read more</Link>
-      </section>
-      <hr />
-      <section>
-        <h1>A Plagues Tale</h1>
-        <Link to="PlagueTalePage">Read more</Link>
-      </section>
-      <hr />
-      <section>
-        <h1>Red Dead Redemption 2</h1>
-        <Link to="RedDeadRedemtion">Read more</Link>
-      </section>
-      <hr />
-      <section>
-        <h1>Skyrim</h1>
-        <Link to="Skyrim">Read more</Link>
-      </section>
-      <hr />
-      <section>
-        <h1>The Last of Us</h1>
-        <Link to="TheLastOfUsPage">Read more</Link>
-      </section>
-      <hr />
-      <section>
-        <h1>The Witcher</h1>
-        <Link to="TheWitcherPager">Read more</Link>
-      </section>
-      <hr />
-      <section>
-        <h1>Shadow of the Tomb Raider</h1>
-        <Link to="TombRaiderPage">Read more</Link>
-      </section>
-      <hr />
-      <section>
-        <h1>Uncharted 4</h1>
-        <Link to="UnchartedPage">Read more</Link>
-      </section>
+      <div className="home-page">
+        <div className="header-home-page">
+          <h1>Electric Games</h1>
+        </div>
 
-      <section className="socials-section">
-        <h3>Join our electric community!</h3>
-        <section className="icon-group">
-          <FaInstagram size={50} className="m-5" />
-          <FaYoutube size={50} className="m-5" />
-          <FaTwitter size={50} className="m-5" />
+        <section className="home-page-paragraph">
+          <p>
+            Adventurous, Cinematic games brought to you by Electric Games Inc.
+            Come join our original worlds and experience our creative stories!
+          </p>
         </section>
-      </section>
+
+        <section className="plagues-tale">
+          <h1>A Plagues Tale</h1>
+          <Link to="PlagueTalePage">Read more</Link>
+        </section>
+
+        <section className="tomb-raider">
+          <h1>Shadow of the Tomb Raider</h1>
+          <Link to="TombRaiderPage">Read more</Link>
+        </section>
+
+        <section className="the-last-of-us">
+          <h1>The Last of Us</h1>
+          <Link to="TheLastOfUsPage">Read more</Link>
+        </section>
+
+        <section className="uncharted">
+          <h1>Uncharted 4</h1>
+          <Link to="UnchartedPage">Read more</Link>
+        </section>
+
+        <section className="character-home-page home-page-sections">
+          <h1>Characters</h1>
+        </section>
+
+        <section className="consoles-home-page home-page-sections">
+          <h1>Consoles</h1>
+        </section>
+
+        <section className="socials-section">
+          <h3>Join our electric community!</h3>
+          <section className="icon-group">
+            <FaInstagram size={50} className="m-5" />
+            <FaYoutube size={50} className="m-5" />
+            <FaTwitter size={50} className="m-5" />
+          </section>
+        </section>
+      </div>
     </>
   );
 };
