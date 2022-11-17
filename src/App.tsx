1affe6fc6d852edec0be1c6d5a2150ef46dd9ff1
Marianneind <marianne.indrebo@gmail.com>
2022-11-17 11:15:00 +0100

    removed socials, added to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import Footer from './components/shared/Footer';
-import SocialsBanner from "./components/shared/SocialsBanner";
-import {HomePage, CharacterPage, ConsolePage, AssassinsCreed, PlagueTalePage, RedDeadRedemtion, Skyrim, TheLastOfUsPage, TheWitcherPage, TombRaiderPage, UnchartedPage} from './pages/index'
-import Navbar from './components/shared/Navbar';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Footer from "./components/shared/Footer";
+import {
+  HomePage,
+  CharacterPage,
+  ConsolePage,
+  AssassinsCreed,
+  PlagueTalePage,
+  RedDeadRedemtion,
+  Skyrim,
+  TheLastOfUsPage,
+  TheWitcherPage,
+  TombRaiderPage,
+  UnchartedPage,
+} from "./pages/index";
+import Navbar from "./components/shared/Navbar";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <div>
-      <Navbar/>
-    </div>
-    
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='CharacterPage' element={<CharacterPage/>}/>
-      <Route path='ConsolePage' element={<ConsolePage/>}/>
-      <Route path='AssassinsCreed' element={<AssassinsCreed/>}/>
-      <Route path='PlagueTalePage' element={<PlagueTalePage/>}/>
-      <Route path='RedDeadRedemtion' element={<RedDeadRedemtion/>}/>
-      <Route path='Skyrim' element={<Skyrim/>} />
-      <Route path='TheLastOfUsPage' element={<TheLastOfUsPage/>}/>
-      <Route path='TheWitcherPager' element={<TheWitcherPage/>}/>
-      <Route path='TombRaiderPage' element={<TombRaiderPage/>}/>
-      <Route path='UnchartedPage' element={<UnchartedPage/>}/>
-    </Routes>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+        </div>
 
-    <SocialsBanner/>
-    <Footer/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="CharacterPage" element={<CharacterPage />} />
+          <Route path="ConsolePage" element={<ConsolePage />} />
+          <Route path="AssassinsCreed" element={<AssassinsCreed />} />
+          <Route path="PlagueTalePage" element={<PlagueTalePage />} />
+          <Route path="RedDeadRedemtion" element={<RedDeadRedemtion />} />
+          <Route path="Skyrim" element={<Skyrim />} />
+          <Route path="TheLastOfUsPage" element={<TheLastOfUsPage />} />
+          <Route path="TheWitcherPager" element={<TheWitcherPage />} />
+          <Route path="TombRaiderPage" element={<TombRaiderPage />} />
+          <Route path="UnchartedPage" element={<UnchartedPage />} />
+        </Routes>
 
-    </BrowserRouter>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,49 +1,59 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { FaInstagram, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const HomePage = () => {
-    return(
-      <> 
-        <section>
-            <h1>Assasins Creed</h1>
-            <Link to="AssassinsCreed">Read more</Link>
-        </section>
-        <hr />
-        <section> 
-            <h1>A Plagues Tale</h1>
-            <Link to="PlagueTalePage">Read more</Link>
-        </section>
-        <hr />
-        <section>
-            <h1>Red Dead Redemption 2</h1>
-            <Link to="RedDeadRedemtion">Read more</Link>  
-        </section>
-        <hr />
-        <section>
-            <h1>Skyrim</h1>
-            <Link to="Skyrim">Read more</Link>
-        </section>
-        <hr />
-        <section>
-            <h1>The Last of Us</h1>
-            <Link to="TheLastOfUsPage">Read more</Link>
-        </section>
-        <hr />
-        <section>
-            <h1>The Witcher</h1>
-            <Link to="TheWitcherPager">Read more</Link>
-        </section>
-        <hr />
-        <section>
-            <h1>Shadow of the Tomb Raider</h1>
-            <Link to="TombRaiderPage">Read more</Link>
-        </section>
-        <hr />
-        <section>
-            <h1>Uncharted 4</h1>
-            <Link to="UnchartedPage">Read more</Link>
-        </section>
-      </>
-    )
-}
+  return (
+    <>
+      <section>
+        <h1>Assasins Creed</h1>
+        <Link to="AssassinsCreed">Read more</Link>
+      </section>
+      <hr />
+      <section>
+        <h1>A Plagues Tale</h1>
+        <Link to="PlagueTalePage">Read more</Link>
+      </section>
+      <hr />
+      <section>
+        <h1>Red Dead Redemption 2</h1>
+        <Link to="RedDeadRedemtion">Read more</Link>
+      </section>
+      <hr />
+      <section>
+        <h1>Skyrim</h1>
+        <Link to="Skyrim">Read more</Link>
+      </section>
+      <hr />
+      <section>
+        <h1>The Last of Us</h1>
+        <Link to="TheLastOfUsPage">Read more</Link>
+      </section>
+      <hr />
+      <section>
+        <h1>The Witcher</h1>
+        <Link to="TheWitcherPager">Read more</Link>
+      </section>
+      <hr />
+      <section>
+        <h1>Shadow of the Tomb Raider</h1>
+        <Link to="TombRaiderPage">Read more</Link>
+      </section>
+      <hr />
+      <section>
+        <h1>Uncharted 4</h1>
+        <Link to="UnchartedPage">Read more</Link>
+      </section>
 
-export default HomePage
+      <section className="socials-section">
+        <h3>Join our electric community!</h3>
+        <section className="icon-group">
+          <FaInstagram size={50} className="m-5" />
+          <FaYoutube size={50} className="m-5" />
+          <FaTwitter size={50} className="m-5" />
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
