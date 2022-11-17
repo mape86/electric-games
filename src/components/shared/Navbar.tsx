3bd7a83f93db2dbd6440b88ebe8d409b63183c8d
mape86 <90511658+mape86@users.noreply.github.com>
2022-11-17 11:50:50 +0100

    Changed fontsize to 5 in navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -4,10 +4,10 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <nav className='d-flex justify-content-around navbar navbar-custom'>
-          <Link className='active text-light fs-3 fw-bold text-decoration-none' to='/'> Home </Link>
-          <Link className='active text-light fs-3 fw-bold text-decoration-none' to='ConsolePage'> Consoles </Link>
-          <Link className='active text-light fs-3 fw-bold text-decoration-none' to='AllGames'> All Games </Link>
-          <Link className='active text-light fs-3 fw-bold text-decoration-none' to='CharacterPage'>Characters</Link>  
+          <Link className='active text-light fs-5 fw-bold text-decoration-none' to='/'> Home </Link>
+          <Link className='active text-light fs-5 fw-bold text-decoration-none' to='ConsolePage'> Consoles </Link>
+          <Link className='active text-light fs-5 fw-bold text-decoration-none' to='AllGames'> All Games </Link>
+          <Link className='active text-light fs-5 fw-bold text-decoration-none' to='CharacterPage'>Characters</Link>  
         </nav>
       )
 } 
