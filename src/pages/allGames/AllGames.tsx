02066edb9bdb74557ce9b0b437979382cf01c864
Sondre Stokkan Spæren <89384544+Sondre-S@users.noreply.github.com>
2022-11-18 13:06:26 +0100

    Updated css

--- src/pages/allGames/AllGames.tsx
+++ src/pages/allGames/AllGames.tsx
@@ -1,12 +1,14 @@
 import GameList from "../../components/games/GameList"
 import "../shared/Pages.css"
+import "./AllGames.css"
+
 
 const AllGames = () => {
 
     return(
         <>
         <section className="page-header game-header-img"></section>
-        <section className="container"></section>
+        <section className="container grid-container"></section>
             <GameList/>
         </>
     )
