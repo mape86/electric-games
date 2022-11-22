1b331b20c65a8b9311dff2a81545ac580e31dcc0
Sondre Stokkan Spæren <89384544+Sondre-S@users.noreply.github.com>
2022-11-22 15:55:52 +0100

    Updated GameInfo.tsx

--- src/pages/gameInfoPages/GameInfo.tsx
+++ src/pages/gameInfoPages/GameInfo.tsx
@@ -1,12 +1,8 @@
-import axios from 'axios';
-import React, {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import IGame from "../../interfaces/IGame";
 import "./GameInfo.css";
-
-interface GameInfoProps {
-  game: IGame;
-}
 
 const classNameMap: Record<string, string> = {
   "A Plague Tale: Requiem": "aplaguetale",
@@ -19,45 +15,56 @@ const classNameMap: Record<string, string> = {
   "The Elder Scrolls V: Skyrim": "skyrim",
 };
 
-const GameInfo = ({ game }: GameInfoProps) => {
-  const gameTitleClassName = classNameMap[game.title];
+const GameInfo = () => {
+  const [game, setGame] = useState<IGame | null>(null);
+  const [error, setError] = useState(false);
 
-  const {title} = useParams()
-  const [post, setPost] = useState({})
+  const { id } = useParams();
+
+  const fetchGame = async () => {
+    try {
+      const { data } = await axios.get(`http://localhost:3000/${id}`); // Må passe på at dette er riktig port til db
+      setGame(data);
+    } catch (e) {
+      setError(true);
+    }
+  };
 
   useEffect(() => {
-    const fetch = async () => {
-      try{
-        const{data} = await axios.get(`http://localhost:3000/${title}`)
-        setPost(data)
-      }catch(error){
-        console.error(error)
-    } 
-    }
-    fetch()
-  }, [])
+    fetchGame();
+  }, []);
+
+  const gameTitleClassName = game ? classNameMap[game.title] : "";
+
+  if (!game && !error) {
+    return <div>Loading gameinfo</div>;
+  }
+
+  if (error) {
+    return <div>Oops, something went wrong. Please try again!</div>;
+  }
 
   return (
     <div className={`${gameTitleClassName} gameinfo-page-wrapper`}>
       <div className="game-info-header ">
-        <h1>{game.title}</h1>
+        <h1>{game?.title}</h1>
         <button>BUY NOW</button>
       </div>
 
       <section className="gameinfo-about-section">
         <h2>About</h2>
-        <p>{game.description}</p>
+        <p>{game?.description}</p>
       </section>
 
       <section className="gameinfo-details-section">
         <h2>Game Details</h2>
         <ul>
-          <li>Title: {game.title}</li>
-          <li>Platform: {game.platform}</li>
-          <li>Release Year: {game.releaseYear}</li>
-          <li>Publisher: {game.publisher}</li>
-          <li>Genre: {game.genre}</li>
-          <li>Price: {game.price}</li>
+          <li>Title: {game?.title}</li>
+          <li>Platform: {game?.platform}</li>
+          <li>Release Year: {game?.releaseYear}</li>
+          <li>Publisher: {game?.publisher}</li>
+          <li>Genre: {game?.genre}</li>
+          <li>Price: {game?.price}</li>
         </ul>
       </section>
 
