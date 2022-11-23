e9e1bbe70714fde929c626d944346798515110bd
Sondre Stokkan Spæren <89384544+Sondre-S@users.noreply.github.com>
2022-11-23 22:05:19 +0100

    Edited console-grid layout.

--- src/components/consoles/GameConsoleItem.tsx
+++ src/components/consoles/GameConsoleItem.tsx
@@ -8,9 +8,8 @@ const GameConsoleItem: FC<IGameConsole> = ({id, model, brand, price, description
     <article className="col-lg-4 col-md-6 col-sm-12 g-4">
         <Card>
             <img className="card-img-top" src={`https://localhost:7003/images/consoles/${image}`} alt="console"/>
-            <h3>{brand}</h3>
-            <h4>{model}</h4>
-            <h5>ID: {id}</h5>
+            <h4 className="text-white">{brand}</h4>
+            <h3>{model}</h3>
             <p>{description}</p>
             <h4>Price NOK: {price}</h4>
         </Card>
