99ae6a7871a32fcd93d2bf4e17e604ce0189ec31
Sondre Stokkan Spæren <89384544+Sondre-S@users.noreply.github.com>
2022-11-23 22:16:21 +0100

    Added decimal to prices in consoles.

--- src/components/consoles/GameConsoleItem.tsx
+++ src/components/consoles/GameConsoleItem.tsx
@@ -8,10 +8,10 @@ const GameConsoleItem: FC<IGameConsole> = ({id, model, brand, price, description
     <article className="col-lg-4 col-md-6 col-sm-12 g-4">
         <Card>
             <img className="card-img-top" src={`https://localhost:7003/images/consoles/${image}`} alt="console"/>
-            <h4 className="text-white">{brand}</h4>
+            <h4>{brand}</h4>
             <h3>{model}</h3>
             <p>{description}</p>
-            <h4>Price NOK: {price}</h4>
+            <h4>Price NOK: {price.toFixed(2)}</h4>
         </Card>
     </article>
     )
